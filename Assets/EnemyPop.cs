9d02b6c26549ae398029c931e22fefc6f66fc1f6
ShotaKasuya <22rd055@ms.dendai.ac.jp>
2022-08-31 00:14:20 +0900

    敵生成

---
+++ Assets/EnemyPop.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPop : MonoBehaviour
+{
+    [SerializeField] private Transform PopPos;
+    [Header("Catsle Settings")]
+    [SerializeField] float HP;
+    [Header("Enemy Set")]
+    [SerializeField] private GameObject[] Enemy = null;
+    [SerializeField] private float[] Frequency;
+
+    private float time;
+    void Start()
+    {
+        time = 0;
+    }
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        GenerateMonster();
+        if (HP == 0) {
+            GameClear();
+        }
+    }
+
+    private void GenerateMonster()
+    {
+        for (int i = 0; Enemy[i] != null; i++)
+            if (Mathf.Approximately(time % Frequency[i], 0.0f))
+            {
+                Instantiate(Enemy[i], PopPos.position,Quaternion.identity);
+            }
+    }
+
+    private void GameClear()
+    {
+        
+    }
+}
