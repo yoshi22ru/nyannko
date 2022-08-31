177ab15e3b5da007aad7dce47237e7fa8dfd0ef1
koma3 <limit307703@gmail.com>
2022-08-31 09:47:59 +0900

    アセット追加とかUI

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Items",menuName = "Items/item")]
+public class Item : ScriptableObject
+{
+    [SerializeField]
+    private string itemName;
+    [SerializeField]
+    private Sprite itemImage;
+
+    public string MyItemName { get => itemName;}
+    public Sprite MyItemImage { get => itemImage;}
+}
---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour,IBeginDragHandler,IDragHandler,IDropHandler,IEndDragHandler
+{
+    private Item item;
+
+    [SerializeField]
+    private Image itemImage;
+
+    private GameObject draggingObj;
+
+    [SerializeField]
+    private GameObject itemImageObj;
+
+    private Transform canvasTransform;
+
+    private Hand hand;
+    public Item MyItem { get => item; private set => item = value; }
+
+    private void Start()
+    {
+        canvasTransform = FindObjectOfType<Canvas>().transform;
+
+        hand = FindObjectOfType<Hand>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (MyItem == null) return;
+        //アイテムのイメージを複製
+        draggingObj = Instantiate(itemImageObj, canvasTransform);
+
+        //複製を最前面に配置
+        draggingObj.transform.SetAsLastSibling();
+
+        //複製元のアイテムを暗くする
+        itemImage.color = Color.gray;
+
+        //仲介人にアイテムを渡す
+        hand.SetGrabbingItem(MyItem);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (MyItem == null) return;
+
+        //複製がポインターを追従するようにする
+        draggingObj.transform.position = hand.transform.position + new Vector3(20,20,0);
+    }
+
+    public void Setitem(Item item)
+    {
+        MyItem = item;
+
+        if(item!=null)
+        {
+            itemImage.color = new Color(1, 1, 1, 1);
+
+            itemImage.sprite = item.MyItemImage;
+
+        }
+        else
+        {
+            itemImage.color = new Color(0, 0, 0, 0);
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //仲介人がアイテムを持っていなかったら早期return
+        if (!hand.IsHavingItem()) return;
+        //仲介人からアイテムを受け取る
+        Item gotItem = hand.GetGrabbingItem();
+
+        //もともと持っていたアイテムを仲介人に渡す
+        hand.SetGrabbingItem(MyItem);
+
+        Setitem(gotItem);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Destroy(draggingObj);
+
+
+        //仲介人からアイテムを受け取る
+        Item gotItem = hand.GetGrabbingItem();
+        Setitem(gotItem);
+    }
+}
