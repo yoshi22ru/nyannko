6da38527d5bcefbeb7dd4ec9f6fa2054ec726517
koma3 <limit307703@gmail.com>
2022-11-03 18:37:42 +0900

    menumanagerへの変更

--- Assets/Scripts/menumaneger.cs
+++ Assets/Scripts/menumaneger.cs
@@ -7,16 +7,17 @@ using UnityEngine.SceneManagement;
 public class menumaneger : MonoBehaviour
 {
     [SerializeField] private GameObject hensei;
-    //[SerializeField] private GameObject back;
-    //[SerializeField] private GameObject start;
+    [SerializeField] private GameObject back;
+    [SerializeField] private GameObject start;
     AudioSource audioSource;
     [SerializeField] private AudioClip click;
+    [SerializeField] private AudioClip backSE;
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         hensei.GetComponent<Button>().onClick.AddListener(Hensei);
-        //back.GetComponent<Button>().onClick.AddListener(back);
-        //start.GetComponent<Button>().onClick.AddListener(start);
+        back.GetComponent<Button>().onClick.AddListener(Back);
+        start.GetComponent<Button>().onClick.AddListener(St);
     }
 
     // Update is called once per frame
@@ -24,5 +25,16 @@ public class menumaneger : MonoBehaviour
     {
         audioSource.PlayOneShot(click);
         SceneManager.LoadScene("Hensei");
+    }
+
+    void Back()
+    {
+        audioSource.PlayOneShot(backSE);
+        SceneManager.LoadScene("title");
+    }
+    void St()
+    {
+        audioSource.PlayOneShot(click);
+        SceneManager.LoadScene("SelectBattle");
     }
 }
